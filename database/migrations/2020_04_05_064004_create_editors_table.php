7f61bfccfce3746c6e21f7bf6e5f62d8040f8b32
RicardoRL <60958169+RicardoRL@users.noreply.github.com>
2020-04-05 16:04:41 -0500

    Add files via upload

---
+++ database/migrations/2020_04_05_064004_create_editors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEditorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('editors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre', 50);
+            $table->string('apepat', 50);
+            $table->string('apemat', 50);
+            $table->date('fecnac');
+            $table->string('correo', 100);
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('editors');
+    }
+}
